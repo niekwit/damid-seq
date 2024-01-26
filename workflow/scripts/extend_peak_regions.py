fd31ef5b9a0273cef85dfdb508d2cb105f10caf5
niekwit <41302859+niekwit@users.noreply.github.com>
2024-01-26 15:54:18 +0000

    fixed some issues

--- workflow/scripts/extend_peak_regions.py
+++ workflow/scripts/extend_peak_regions.py
@@ -18,25 +18,27 @@ with open(cs) as f:
        chrom_sizes[key] = int(value)
 
 extended_peaks = 0
+count = 1
 
 with open(bed, "r") as bed_in, open(ext_bed, "w") as bed_out:
     while True:
-        # For each line, reset extend_by value
+        # For each line, reset extend_by value 
+        # (may be set to 0 if region is too large)
         extend_by = int(snakemake.params["e"])
         
         # Read in one line at a time
-        line = bed.readline()
+        line = bed_in.readline()
         
         # Stop if end of file is reached
         if not line:
             break
         
-        # Load line into variables (everything after 4th column is not needed)
+        # Load line into variables
         chrom, start, end, num, *args = line.split()
         
         # Check how many regions are overlapping
         # Keep only regions that come from at least k regions
-        if num < k:
+        if int(num) < k:
             continue # Skip to next line
         
         # Check if region is short enough to extend 
@@ -44,22 +46,28 @@ with open(bed, "r") as bed_in, open(ext_bed, "w") as bed_out:
         start = int(start)
         end = int(end)
         size = end - start
-        if size <= max_size:
+        if size >= max_size:
             extend_by = 0
                        
-        # Extend the regions by 100 bp on each side
+        # Extend the regions by x bp on each side (read from config.yaml)
         start = start - extend_by
         end = end + extend_by
+        if extend_by != 0:
+            extended_peaks += 1
         
         # Make sure that the extended regions do not extend past the begin/end of the chromosome
         if start < 0:
             start = 0
         if end > chrom_sizes[chrom]:
             end = chrom_sizes[chrom]
+        
+        # Generate name for extended region
+        name = f"peak_{count}"
+        count += 1
             
-        # Write extended region to output file (4th column is from how many replicates the peak came from)
-        bed_out.write(f"{chrom}\t{start}\t{end}\t{num}\n")
-        extended_peaks += 1
+        # Write extended region to output file
+        bed_out.write(f"{chrom}\t{start}\t{end}\t{name}\t0\t{num}\n")
+        
         
 print(f"Done...\nExtended {extended_peaks} peaks in {bed}")
 
