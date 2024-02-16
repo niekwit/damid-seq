41407292d10a39c584f485a376d601e1e31b5f35
niekwit <41302859+niekwit@users.noreply.github.com>
2024-02-16 15:10:33 +0000

    fixed issues

--- workflow/scripts/peak_annotation_plots.R
+++ workflow/scripts/peak_annotation_plots.R
@@ -6,45 +6,48 @@ sink(log, type = "message")
 # Load libraries
 library(GenomicFeatures)
 library(ChIPseeker)
+library(tidyverse)
 
 # Load Snakemake parameters
 bed.files <- snakemake@input[["bed"]]
-txdb <- snakemake@input[["txdb"]]
+gtf <- snakemake@input[["gtf"]]
 
 # Load annotation database
-load(txdb)
+txdb <- makeTxDbFromGFF(gtf)
 
-# Annotation list of all bed files
-peakAnnoList <- lapply(files, annotatePeak, TxDb=txdb,
-                       tssRegion=c(-3000, 3000), verbose=FALSE)
+# Add sample names to bed files
+samples <- sub(".*\\/([^\\/]+)\\.extended.bed", "\\1", bed.files)
+names(bed.files) <- samples
 
-# Feature distribution
-pdf(snakemake@output[["fd"]])
-plotAnnoBar(peakAnnoList)
+# Annotate bed files
+peakAnnoList <- lapply(bed.files,
+                        annotatePeak,
+                        TxDb = txdb,
+                        tssRegion = c(-3000, 3000)
+                        )
+
+# Plot binding relative to TSS
+pdf(snakemake@output[["tss"]])
+plotDistToTSS(peakAnnoList,
+              title =  "Distribution of peaks\nrelative to TSS") +
+  theme(axis.line.y = element_line(size = 0),
+        axis.line.x = element_line(size = 0.5),
+        panel.border = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank(),
+        text = element_text(size = 20))
 dev.off()
 
-# Distance to TSS
-pdf(snakemake@output[["dt"]])
-plotDistToTSS(peakAnnoList)
+# Plot annotation bar
+pdf(snakemake@output[["bar"]])
+plotAnnoBar(peakAnnoList) +
+  theme(axis.line.y = element_line(size = 0),
+        axis.line.x = element_line(size = 0.5),
+        panel.border = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank(),
+        text = element_text(size = 20))
 dev.off()
-
-# Pathway analysis
-genes <- lapply(peakAnnoList, function(i) as.data.frame(i)$geneId)
-names(genes) <- sub("_", "\n", names(genes))
-compKEGG <- compareCluster(geneCluster = genes,
-                           fun = "enrichKEGG",
-                           pvalueCutoff = 0.05,
-                           pAdjustMethod = "BH")
-
-pdf(snakemake@output[["pa"]])
-dotplot(compKEGG,
-        showCategory = 10,
-        title = "KEGG Pathway Enrichment Analysis")
-dev.off()
-
-# Overlap of peaks and annotated genes
-genes= lapply(peakAnnoList, function(i) as.data.frame(i)$geneId)
-vennplot(genes)
 
 # Close redirection of output/messages
 sink(log, type = "output")
