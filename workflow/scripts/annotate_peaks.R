a5b11973f3f582740a4c9b09b8878cdc515e4df8
niekwit <41302859+niekwit@users.noreply.github.com>
2024-02-16 15:09:57 +0000

    only annotates peaks, no plotting

--- workflow/scripts/annotate_peaks.R
+++ workflow/scripts/annotate_peaks.R
@@ -6,24 +6,31 @@ sink(log, type = "message")
 # Load libraries
 library(GenomicFeatures)
 library(ChIPseeker)
+library(tidyverse)
 
 # Load Snakemake parameters
-bed <- snakemake@input[["bed"]]
+DB <- snakemake@input[["adb"]]
 gtf <- snakemake@input[["gtf"]]
 txt <- snakemake@output[["txt"]]
+bed.file <- snakemake@input[["bed"]]
 
 # Load annotation database
 txdb <- makeTxDbFromGFF(gtf)
 
 # Annotate bed file
-peakAnno <- annotatePeak(bed,
+peakAnno <- annotatePeak(bed.file,
                          tssRegion = c(-3000, 3000),
-                         TxDb = txdb)
+                         TxDb = txdb
+                        )
 
 # Tidy up annotation data
 df <- as.data.frame(peakAnno@anno@elementMetadata@listData)
-names(df)[1] <- "peak_id"
-df <- df[, -c(2:4)]
+names(df)[1:3] <- c("peak_id", "fold_enrichment", "strand")
+
+# Add gene names and gene biotype to annotation
+load(DB)
+df <- df %>%
+  left_join(edb, by = "geneId")
 
 # Write annotation to file
 write.table(df,
