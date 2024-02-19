2af1665bca318d65a047a067f8262e1ffbf7b3a4
niekwit <41302859+niekwit@users.noreply.github.com>
2024-02-19 12:41:56 +0000

    Initial commit

---
+++ workflow/scripts/convert_bed2fasta.py
@@ -0,0 +1,68 @@
+"""
+Converts a bed file to a fasta file. 
+As DamID is not strand specific, the sequence 
+will be extracted from the positive strand only.
+"""
+import pandas as pd
+from Bio import SeqIO
+#from Bio.Seq import Seq
+
+def load_bed(bed):
+    """
+    Load bed file as pandas df
+    """
+    # Load bed file as pandas df and add header names
+    # This is not the most efficient way to do this (optimise later)
+    bed = pd.read_csv(bed, sep="\t", header=None)
+    bed.columns = ["chr", "start", "end", "name", "strand", "score"]
+    
+    return bed
+
+
+def load_fasta(fasta):
+    """
+    Load fasta file and return dict with chr as key and sequence as value
+    """
+    chr_seq = {}
+    for chr_ in SeqIO.parse(fasta, "fasta"):
+        chr_seq[chr_.id] = chr_.seq
+    
+    return chr_seq
+
+
+def write_dict2fasta(d, out):
+    """
+    Write dict to fasta file
+    """
+    with open(out, "w") as f:
+        for name, seq in d.items():
+            f.write(f">{name}\n{seq}\n")
+
+
+def main(bed, chr_seq):
+    # Dict to store sequence per peak
+    peak_seq = {}
+
+    # For each region in bed file, extract sequence and write to fasta file
+    for row in bed.itertuples(index=False):
+        chr = row.chr
+        start = row.start
+        end = row.end
+        name = row.name
+
+        # Extract sequence
+        seq = chr_seq[chr][start:end]
+        
+        # Store sequence in dict
+        peak_seq[f"{name}_{chr}_{start}_{end}"] = seq
+
+    # Write to fasta file
+    write_dict2fasta(peak_seq, snakemake.output["out"])
+           
+if __name__ == "__main__":
+    bed = load_bed(snakemake.input["bed"], snakemake.output["out"])
+    chr_seq = load_fasta(snakemake.input["fasta"])
+    
+    main(bed, chr_seq)
+    
+    
---
+++ workflow/scripts/create_background_fasta.py
@@ -0,0 +1,73 @@
+import random
+import convert_bed2fasta as utils
+import pandas as pd
+
+bed = utils.load_bed(snakemake.input["bed"])
+fasta = utils.load_fasta(snakemake.input["fasta"]) # whole genome fasta
+chrom_sizes = snakemake.input["cs"]
+chrom_sizes = pd.read_csv(chrom_sizes, 
+                          sep="\t", 
+                          header=None,
+                          names=["chr", "size"])
+
+# Remove non-canonical chromosomes (beginning with GL, KI) from chrom_sizes
+chrom_sizes = chrom_sizes[~chrom_sizes["chr"].str.contains("GL|KI")]
+
+# Add weight to each chromosome based on size
+chrom_sizes["weight"] = chrom_sizes["size"] / chrom_sizes["size"].sum()
+
+# Get chromosome names from fasta dict
+chr_names = chrom_sizes["chr"].tolist()
+
+# Get value lengths of each key in fasta dict
+chr_lengths = chrom_sizes["size"].tolist()
+
+# Create dict with chromosome names as keys and chromosome lengths as values
+chr_seq = dict(zip(chr_names, chr_lengths))
+
+def random_seq(length):
+    """
+    Returns random genomic sequence with no Ns in sequence
+    Length of sequence is the same as the input peaks
+    """
+    while True:
+        # Randomly select chromosome
+        chr = random.choices(population=chrom_sizes["chr"],
+                             weights=chrom_sizes["weight"],
+                             k=1)[0]
+        
+        # Get chromosome length
+        chr_length = chr_seq[chr]
+        
+        # Randomly select end and start position
+        end = random.randint(1, chr_length)
+        start = end - length
+        
+        # Extract sequence
+        seq = fasta[chr][start:end]
+        
+        # Check if sequence contains any Ns, if so pick another sequence
+        if "N" not in seq:
+            return seq, chr, start, end
+
+# Dict to store random regions
+random_regions = {}
+
+# For each peak in bed file, create a random sequence with the same length
+for row in bed.itertuples(index=False):
+    # Get peak length
+    start = int(row.start)
+    end = int(row.end)
+    name = row.name
+    peak_number = name.split("_")[1]
+    length = end - start
+
+    # Get random sequence, chromosome, start position and end position
+    _seq, _chr, _start, _end = random_seq(length)
+        
+    # Store sequence in dict
+    random_regions[f"random_{peak_number}_{_chr}_{_start}_{_end}"] = _seq
+
+# Write random regions to fasta file
+utils.write_dict2fasta(random_regions, snakemake.output["out"])
+
