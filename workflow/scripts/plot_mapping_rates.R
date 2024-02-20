46db6271b72c3429e4726088818591c5b80239bb
niekwit <41302859+niekwit@users.noreply.github.com>
2024-02-20 13:21:55 +0000

    fixed empty data frame

--- workflow/scripts/plot_mapping_rates.R
+++ workflow/scripts/plot_mapping_rates.R
@@ -11,7 +11,7 @@ library(cowplot)
 # These log files are empty but use them to get 
 # log dirs and find proper log files
 log.files <- snakemake@input["log"]
-log.dirs <- dirname(log.files)
+log.dirs <- unlist(lapply(log.files, dirname))
 
 # Get proper log files (log file starts with pipeline- and ends with .log)
 log.files <- list.files(log.dirs,
@@ -20,29 +20,29 @@ log.files <- list.files(log.dirs,
 
 # Data frame to store mapping rates of all experiments
 mapping.rates.all <- data.frame(sample = character(),
-                              overall_mapping_rate = numeric())
+                                overall_mapping_rate = numeric())
 
 # Extract mappings rates from each log file (one log file per experiment)
 for (i in seq_along(log.files)) {
   # Get dir name (dir = experiment)
   dir <- basename(log.dirs[[i]])
-  
+
   # Read part of log file that contains mapping data
   log.section <- system(paste("sed '/Reading data files/,/Reading GATC file/!d'", log.files[[i]]), 
                         intern = TRUE)
-  
+
   # Get sample names and add dir name
   sample.names <- log.section[grepl("Now working on ", log.section)]
   sample.names <- str_replace(sample.names, "Now working on ", "")
   sample.names <- paste0(dir, "_", str_replace(sample.names, " ...", ""))
-  
+
   # Get line numbers where overall mapping rate is printed
-  rate.lines <- grep("Now working on ", log.section) + 6 # the mapping rate is 6 lines down
-  
+  rate.lines <- grep("% overall alignment rate", log.section)
+
   # Extract mapping rates
   mapping.rates <- data.frame(sample = sample.names,
                                 overall_mapping_rate = as.numeric(str_extract(log.section[rate.lines], "\\d+\\.\\d+")))
-  
+
   # Add to data frame with all data
   mapping.rates.all <- rbind(mapping.rates.all, mapping.rates)
 }
@@ -52,7 +52,7 @@ p <- ggplot(mapping.rates.all, aes(x = sample, y = overall_mapping_rate)) +
   geom_bar(stat = "identity",
            position = "dodge",
            colour = "black",
-           fill="aquamarine4") +
+           fill = "aquamarine4") +
   theme_cowplot(18) +
   theme(plot.margin = margin(t = 0.5, r = 1.5, b = 0.5, l = 0.5, unit = "cm")) +
   labs(x = NULL,
@@ -63,7 +63,6 @@ p <- ggplot(mapping.rates.all, aes(x = sample, y = overall_mapping_rate)) +
 
 # Save to file
 ggsave(snakemake@output[["pdf"]], p)
-
 
 # Close redirection of output/messages
 sink(slog, type = "output")
