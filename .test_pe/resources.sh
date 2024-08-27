4381352b6c4c4735e84028a09f1699b7285f04ff
niekwit <41302859+niekwit@users.noreply.github.com>
2024-08-27 14:27:14 +0100

    tidy up chromosome names

--- .test_pe/resources.sh
+++ .test_pe/resources.sh
@@ -1,2 +1,3 @@
 # Create fasta file with regions were just peaks are located
-samtools faidx Homo_sapiens.GRCh38.dna.primary_assembly.fa -r regions.txt | pigz > Homo_sapiens.GRCh38.dna.primary_assembly_subset.fa.gz
+samtools faidx Homo_sapiens.GRCh38.dna.primary_assembly.fa -r regions.txt | sed 's/:.*$//' |  pigz > Homo_sapiens.GRCh38.dna.primary_assembly_subset.fa.gz
+
