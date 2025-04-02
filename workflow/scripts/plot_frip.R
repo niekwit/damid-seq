9fe66b06549280d2507c1c41e2f987288bf59fe1
niekwit <41302859+niekwit@users.noreply.github.com>
2025-04-02 15:27:43 +0100

    simplified

--- workflow/scripts/plot_frip.R
+++ workflow/scripts/plot_frip.R
@@ -6,37 +6,14 @@ sink(log, type = "message")
 library(tidyverse)
 library(cowplot)
 
-# Plot bar graph of FRiP values
-total_read_counts <- snakemake@input[["total_read_count"]]
-peak_read_counts <- snakemake@input[["peak_read_count"]]
 
-# Create df to store FRiP values
-df <- data.frame(sample = character(),
-                 read_in_peaks = numeric(),
-                 total_reads = numeric(),
-                 fraction_of_reads_in_peaks = numeric())
-
-# Add FRiP values to df
-for (i in seq_along(total_read_counts)) {
-  sample <- basename(total_read_counts[i]) %>% str_remove(".total.count")
-  sample <- paste(basename(dirname(total_read_counts[i])), sample, sep="_")
-  total_reads <- as.numeric(read_lines(total_read_counts[i]))
-  peak_reads <- as.numeric(read_lines(peak_read_counts[i]))
-  frip <- peak_reads / total_reads
-  
-  df <- df %>%
-    add_row(sample = sample,
-            read_in_peaks = peak_reads,
-            total_reads = total_reads,
-            fraction_of_reads_in_peaks = frip)
-}
-
-# Save df to file
-write.csv(df, snakemake@output[["csv"]], row.names = FALSE)
+# Read FRIP values from input file
+csv <- snakemake@input[["csv"]]
+df <- read.csv(csv, header = TRUE)
 
 # Create plot
-p <- ggplot(df, aes(x = sample, 
-                    y = fraction_of_reads_in_peaks)) +
+p <- ggplot(df, aes(x = name, 
+                    y = fraction_in_peaks)) +
   geom_bar(stat = "identity", 
            fill = "#419179",
            colour = "black") +
