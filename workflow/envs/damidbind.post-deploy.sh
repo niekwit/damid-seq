a73acac7b017a3e040aef644c0db59b09ecbf629
niekwit <41302859+niekwit@users.noreply.github.com>
2026-06-23 13:59:05 +0100

    pin version to v1.1.1

--- workflow/envs/damidbind.post-deploy.sh
+++ workflow/envs/damidbind.post-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 Rscript -e 'install.packages("BioVenn", repos="https://cloud.r-project.org"); stopifnot(requireNamespace("BioVenn", quietly=TRUE))'
-Rscript -e 'remotes::install_github("marshall-lab/damidBind", upgrade="never"); stopifnot(requireNamespace("damidBind", quietly=TRUE))'
+Rscript -e 'remotes::install_github("marshall-lab/damidBind", ref="v1.1.1", upgrade="never"); stopifnot(requireNamespace("damidBind", quietly=TRUE))'
